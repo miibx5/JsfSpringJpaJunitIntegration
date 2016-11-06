c74f68a66eb8662c5e88e1f5fff58206d04f4eca
miibx5 <miibx5@gmail.com>
2016-11-06 20:07:52 -0200

    Adicionado o laput e o spring funcionando

---
+++ src/main/java/br/com/edersystems/jsfspringjpajunitintegration/model/entities/Person.java
@@ -0,0 +1,143 @@
+/*
+ *  Projeto.......: JsfSpringJpaJunitIntegration
+ *  Developer.....: Éder Luciano da Costa
+ *  Copyright.....: 2016
+ *  Criação.......: 06/11/2016, 19:50:00
+ *  Revisao.......: $Rev:$, $Id:$
+ *  Codificacão...: UTF-8 (Utilizado na criação do arquivo)
+ *  @author.......: Luciano
+ *  .....................................................................................
+ *  Éder L. Costa - © Copyright 2016 - All Rights Reserved
+ *  .....................................................................................
+ */
+package br.com.edersystems.jsfspringjpajunitintegration.model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Luciano
+ */
+@Entity
+@Table(name = "persons")
+public class Person implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name_of_person", nullable = false, length = 120)
+    private String name;
+
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    @Column(name = "registration_date", nullable = false)
+    private Date registrationDate;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @Column(name = "inativation_date")
+    private Date inativationDate;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    public Person()
+    {
+        this(null, null, null, null, null);
+    }
+
+    public Person(Long id, String name, Date registrationDate, Date inativationDate, String email)
+    {
+        this.id = id;
+        this.name = name;
+        this.registrationDate = registrationDate;
+        this.inativationDate = inativationDate;
+        this.email = email;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Date getRegistrationDate()
+    {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate)
+    {
+        this.registrationDate = registrationDate;
+    }
+
+    public Date getInativationDate()
+    {
+        return inativationDate;
+    }
+
+    public void setInativationDate(Date inativationDate)
+    {
+        this.inativationDate = inativationDate;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if(!(object instanceof Person))
+        {
+            return false;
+        }
+        Person other = (Person)object;
+        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+    }
+
+    @Override
+    public String toString()
+    {
+        return "br.com.edersystems.jsfspringjpajunitintegration.model.entities.Person[ id=" + id + " ]";
+    }
+
+}
